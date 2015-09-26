86ab42b063c138439a48c42bc32d255d93bd4c55
b3mb4m <b3mb4m@gmail.com>
2015-09-26 18:57:50 +0300

    Create catalan.py

---
+++ CatalaNumbers/catalan.py
@@ -0,0 +1,10 @@
+#Coded By B3mB4m
+#C(n) = (2n)!/(n+1)!n! n => 0
+#And we want first x10 number ..
+
+def fac(number):
+	return eval("*".join([str(x) for x in xrange(1,int(number)+1)][::-1]))
+	#https://github.com/b3mb4m/Algorithms/blob/master/factorial/fac.py
+
+for n in xrange(1, 11):
+	print fac( 2*n)/(fac( n+1)*fac( n)) 
